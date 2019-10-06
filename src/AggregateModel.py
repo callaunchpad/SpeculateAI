17cc9dced769df55a7ef16f50537314a40150c07
Joey Kraut <joseph.h.kraut@gmail.com>
2019-10-05 22:51:02 -0700

    Added initial structure to the aggregate model class

---
+++ src/AggregateModel.py
@@ -0,0 +1,121 @@
+import tensorflow as tf
+
+"""
+This code defines how we aggregate natural language, time series, and output graphs
+into a single, cohesive computation graph.
+
+The code below expects objects of the same structure as defined in BaseModel.py
+"""
+
+class AggregateModel():
+    def __init__(self, nlp_model, tsa_model, downstream_model, combination=tf.concat,
+                 combination_args=[], label_shape=[1], is_classifier=True, tsa_in_tf=True):
+        """
+        Initializes the full computation graph such that we have a complete aggregated model
+
+        :param nlp_model: The nlp model to use in graph combination
+        :param tsa_model: The time series model to use in graph combination
+        :param downstream_model: The model that will take in combined outputs of nlp and tsa models
+        :param combination: The method by which we combine the nlp and tsa outputs
+        :param combination_args: The extra arguments needed for the combination method
+        :param label_shape: The shape our labels will take on without batch size
+        :param is_classifier: Whether or not the output should be interpreted as classification or regression
+        :param tsa_in_tf: Whether or not the time series model is a tensorflow graph
+        """
+
+        self.is_classifier = is_classifier
+        self.combine_models(nlp_model, tsa_model, downstream_model, combination, combination_args, label_shape, tsa_in_tf)
+
+    def combine_models(self, nlp_model, tsa_model, downstream_model, combination, combination_args, label_shape, tsa_in_tf):
+        """
+        Builds the computation graph by connecting the component graphs in the appropriate manner
+
+        :param nlp_model: The natural language model used in aggregated graph
+        :param tsa_model: The time series model used in aggregated graph
+        :param downstream_model: The part of the aggregated graph that receives combined nlp and tsa outputs
+        :param combination: The tensorflow operation used to combine the nlp and tsa outputs
+        :param combination_args: The extra arguments needed for the combination method
+        :param label_shape: The shape our labels will take on without batch size
+        :return: None
+        """
+
+        # The inputs and output from the nlp model
+        self.nlp_inputs = nlp_model.inputs
+        self.nlp_outputs = nlp_model.output
+
+        # The inputs and outputs from the time series model
+        if tsa_in_tf:
+            self.tsa_inputs = tsa_model.inputs
+            self.tsa_outputs = tsa_model.output
+        else:
+            # If our tsa model is a not a TF graph (e.g. logistic regression)
+            self.tsa_outputs = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+
+        # The combination of the nlp and time series outputs
+        downstream_input = combination([self.nlp_outputs, self.tsa_outputs], *combination_args)
+
+        # The input the downstream model
+        downstream_model.inputs = downstream_input
+
+        # The output of the downstream model
+        self.downstream_output = downstream_model.output
+
+        # The labels fed in for training the aggregated model
+        self.model_labels = tf.placeholder(shape=[None] + label_shape, dtype=tf.float32)
+
+        # The loss for the aggregated model
+        if self.is_classifier:
+            self.loss = tf.losses.softmax_cross_entropy(self.model_labels, self.downstream_output)
+        else:
+            self.loss = tf.losses.mean_squared_error(self.model_labels, self.downstream_output)
+
+
+        # Setting the trainable variables for the optimizer, originally just fine tuning
+        downstream_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="DOWN")
+
+        # The training operation called to perform a gradient step
+        self.train_op = tf.train.AdamOptimizer().minimize(self.loss, var_list=downstream_vars)
+
+
+    def train_step(self, nlp_inputs, tsa_inputs, labels, sess, tsa_callback=None):
+        """
+        Performs a training step of the trainable variables
+
+        :param nlp_inputs: The inputs to the NLP branch of the model
+        :param tsa_inputs: The inputs to the time series branch of the model
+        :param labels: The labels on which to evaluate loss and perform gradient calculations
+        :param sess: The tensorflow session in which to run the evaluation and backpropagation
+        :param tsa_callback: The callback of the TSA model if we are not using a tensorflow implemented
+        time series model, this allows us to pass in a generic function to evaluate, for example, an
+        ARIMA model
+        :return: The loss on this given training step
+        """
+
+        raise NotImplementedError()
+
+    def get_loss(self, nlp_inputs, tsa_inputs, labels, sess, tsa_callback=None):
+        """
+        Computes the loss on the given set of inputs and labels
+
+        :param nlp_inputs: The inputs to the NLP branch of the model
+        :param tsa_inputs: The inputs to the TSA branch of the model
+        :param labels: The labels used in computing the loss
+        :param sess: The tensorflow session in which to evaluate the graph
+        :param tsa_callback: The callback of the TSA model (see train_step docstring for more info)
+        :return: The loss on this given (inputs, labels) set
+        """
+
+        raise NotImplementedError()
+
+    def predict(self, nlp_inputs, tsa_inputs, sess, tsa_callback):
+        """
+        Runs inference on the aggregated model for given inputs
+
+        :param nlp_inputs: The inputs to the NLP branch of the model
+        :param tsa_inputs: The inputs to the TSA branch of the model
+        :param sess: The tensorflow session in which to evaluate the graph
+        :param tsa_callback: See train_step docstring
+        :return: The predicted values after inference
+        """
+
+        raise NotImplementedError()
--- src/BaseModel.py
+++ src/BaseModel.py
@@ -1,68 +1,78 @@
 import tensorflow as tf
 
 """
-This is the basic structure of the models in this project
+This is the basic structure of the models in this project.
+In this context, model is a substructure of the overall aggregated graph
 """
 
-class Model:
-    def __init__(self, hyper_parameters):
+class BaseModel:
+    def __init__(self, hyper_parameters={}):
         """
         Sets up any hyper-parameters that we may need for the model and builds the graph
         """
-        self.build_graph(hyper_parameters)
+        # The following sets the tensorflow scope for this particular model
+        # It is important that the model have one of a few scopes:
+        #       - NLP: for models evaluating the NLP branch
+        #       - TSA: for models evaluating the TSA branch
+        #       - DOWN: for models evaluating the combined time series and nlp outputs
+        # These scopes are used in aggregation to allow for end-to-end training or downstream fine tuning
+        scope = "NLP"
+        self.build_graph(scope, hyper_parameters)
 
-    def build_graph(self, hyper_parameters):
+    def build_graph(self, scope="NLP", hyper_parameters={}):
         """
         Builds the computation graph this model will be working with.
 
         Will build the graph with specific fields:
             - self.inputs: an input tf placeholder that will be called
             - self.output: a tensorflow operation to be evaluated for network output
+            - self.labels: a tensorflow placeholder for holding labels during training
             - self.loss: a tensorflow operation that evaluates to the loss given inputs and outputs
             - self.train_op: a training operation we will call during training
 
         :return: None
         """
-
-        self.inputs = None
-        self.output = None
-        self.loss = None
-        self.train_op = None
+        with tf.variable_scope(scope):
+            self.inputs = None
+            self.output = None
+            self.labels = None
+            self.loss = None
+            self.train_op = None
 
         return
 
-    def train_step(self, inputs, outputs, sess):
+    def train_step(self, inputs, labels, sess):
         """
         Performs a training step using self.train_op in the passed session.
 
         :param inputs: The inputs to forward propagate
-        :param outputs: The outputs used in computing the loss function
+        :param labels: The outputs used in computing the loss function
         :param sess: The tensorflow session used for evaluating the training operation
         :return: The loss on <inputs> and <outputs>
         """
 
         feed_dict = {
             self.inputs: inputs,
-            self.outputs: outputs
+            self.labels: labels
         }
 
         loss_value, _ = sess.run([self.loss, self.train_op], feed_dict=feed_dict)
 
         return loss_value
 
-    def get_loss(self, inputs, outputs, sess):
+    def get_loss(self, inputs, labels, sess):
         """
         Evaluates the loss on the given inputs and outputs in the given TF session
 
         :param inputs: The inputs to forward propagate
-        :param outputs: The outputs used in computing the loss function
+        :param labels: The outputs used in computing the loss function
         :param sess: The tensorflow session used for evaluating the loss operation
         :return: The loss on <inputs> and <outputs>
         """
-        
+
         feed_dict = {
             self.inputs: inputs,
-            self.output: outputs
+            self.output: labels
         }
 
         loss_value = sess.run(self.loss, feed_dict=feed_dict)

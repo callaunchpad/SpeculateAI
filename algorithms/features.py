26ed6a3884b97df3845cc6c904fd80d4042c6bbf
Kevin <chevin_ken@berkeley.edu>
2019-10-17 18:19:22 -0700

    Testing skeleton

--- algorithms/features.py
+++ algorithms/features.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import numpy as np
-import time
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import KFold
 
 def MA(ts, window):
     # MOVING AVERAGE FEATURE
---
+++ algorithms/test.py
@@ -0,0 +1,27 @@
+from features import *
+
+def main():
+    testMA()
+
+
+def testMA():
+    return
+
+def testEMA():
+    return
+
+def testMACD():
+    return
+
+def testBB():
+    return
+
+def testRSI():
+    return
+
+def testVMA():
+    return
+
+def test_labels():
+    return
+

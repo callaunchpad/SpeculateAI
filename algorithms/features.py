8049cef9f758bafe95ce94ac4dff987b63c9798a
Kevin <chevin_ken@berkeley.edu>
2019-10-17 17:23:55 -0700

    Finished BB, MACD, getting labels

--- algorithms/features.py
+++ algorithms/features.py
@@ -10,11 +10,28 @@ def MA(ts, window):
 def EMA(ts, window):
     # MOVING AVERAGE FEATURE
 
-def MACD(ts, window):
+def MACD(ts):
     # MOVING AVERAGE FEATURE
+    #MACD is just subtracting EMA with a 12 day window from EMA with a 26 day window
+    return EMA(ts, 12) - EMA (ts, 26)
 
-def BB(ts, window):
-    # MOVING AVERAGE FEATURE
+
+def BB(ts, stddev, window):
+    # BOLLINGER BAND FEATURE
+
+    # upper bound
+    bolu = []
+    # lower bound
+    boll = []
+
+    ma = MA(ts, window)
+    std = std_window(ts, window)
+
+    for i in range(len(ma)):
+        bolu.append(ma[i] + stddev * std[i])
+        boll.append(ma[i] - stddev * std[i])
+
+    return bolu, boll
 
 def RSI(ts, window):
     # MOVING AVERAGE FEATURE
@@ -23,3 +40,23 @@ def VMA(ts, window):
     # VOLUME MOVING AVERAGE FEATURE
 
 def get_labels(ts):
+    # -1 is go down, 0 is stay the same, 1 is go up
+    # First input is always 0
+
+    labels = [0]
+    for i in range(1, len(ts)):
+        prev = ts[i - 1]
+        curr = ts[i]
+        if prev > curr:
+            labels.append(-1)
+        elif prev == curr:
+            labels.append(0)
+        elif prev < curr:
+            labels.append(1)
+    return labels
+
+
+def std_window(ts, window):
+    res = ts.rolling(window, 1).std()
+
+    return res

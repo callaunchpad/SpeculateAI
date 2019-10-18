1c8c00e14bc00ee6f9989ad3d44f0d9e4fb1d10b
William Wang <wwang0430@berkeley.edu>
2019-10-17 17:15:14 -0700

    feature skeleton

---
+++ algorithms/features.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import time
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
+
+def MA(ts, window):
+    # MOVING AVERAGE FEATURE
+
+def EMA(ts, window):
+    # MOVING AVERAGE FEATURE
+
+def MACD(ts, window):
+    # MOVING AVERAGE FEATURE
+
+def BB(ts, window):
+    # MOVING AVERAGE FEATURE
+
+def RSI(ts, window):
+    # MOVING AVERAGE FEATURE
+
+def VMA(ts, window):
+    # VOLUME MOVING AVERAGE FEATURE
+
+def get_labels(ts):

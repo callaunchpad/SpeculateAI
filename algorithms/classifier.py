ad45a47ae5dbaf3c38557493d497d82dc560227b
William Wang <wwang0430@berkeley.edu>
2019-10-20 14:33:38 -0700

    log regression

--- algorithms/classifier.py
+++ algorithms/classifier.py
@@ -9,4 +9,4 @@ def logModel(features, target):
     for i in features:
         i = np.asarray(i)
         X = np.hstack((X, np.transpose([i])))
-    return LogisticRegression(random_state=0, solver='lbfgs', multi_class = 'multinomial').fit(X, target)
+    return LogisticRegression().fit(X, target)

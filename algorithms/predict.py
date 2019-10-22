d8b4dbd708ae42d33bcf589f62929a534fd9ec67
William Wang <wwang0430@berkeley.edu>
2019-10-21 17:00:41 -0700

    split into test/train

--- algorithms/predict.py
+++ algorithms/predict.py
@@ -1,34 +1,53 @@
 from models import *
 
+test_percent = 50 # Percent of data used for verification
+
 df_DJIA = pd.read_csv("../data/DJIA_table.csv")
-close = df_DJIA["Close"]
+split = int(len(df_DJIA["Close"]) * (1 - test_percent/100))
+close = df_DJIA["Close"][:split]
+test = df_DJIA["Close"][split:]
 
-window = 10
-feature_list = []
-target = get_labels(close)[4:]
 
-bb1, bb2 = BB(close, window)
+def feature_list(data, window):
+    features = []
 
-feature_list.append(close[:len(close) - 5])
-feature_list.append(close[1:len(close) - 4])
-feature_list.append(close[2:len(close) - 3])
-feature_list.append(close[3:len(close) - 2])
-feature_list.append(close[4:len(close) - 1])
-feature_list.append(close[5:])
-feature_list.append(MA(close, window).tolist()[5:])
-feature_list.append(EMA(close, window).tolist()[5:])
-feature_list.append(MACD(close).tolist()[5:])
-feature_list.append(bb1[5:])
-feature_list.append(bb2[5:])
-feature_list.append([bb1[i] - bb2[i] for i in range(5, len(bb1))])
-feature_list.append(RSI(close, window).tolist()[5:])
-feature_list.append(VMA(close, window).tolist()[5:])
+    bb1, bb2 = BB(data, window)
+    bb1, bb2 = bb1, bb2
 
-model = logModel(feature_list, target)
+    features.append(data[:len(data) - 5])
+    features.append(data[1:len(data) - 4])
+    features.append(data[2:len(data) - 3])
+    features.append(data[3:len(data) - 2])
+    features.append(data[4:len(data) - 1])
+    features.append(data[5:])
+    features.append(MA(data, window).tolist()[5:])
+    features.append(EMA(data, window).tolist()[5:])
+    features.append(MACD(data).tolist()[5:])
+    features.append(bb1[5:])
+    features.append(bb2[5:])
+    features.append([bb1[i] - bb2[i] for i in range(5, len(bb1))])
+    features.append(RSI(data, window).tolist()[5:])
+    features.append(VMA(data, window).tolist()[5:])
 
-X = np.zeros((len(target),1))
-for i in feature_list:
-    i = np.asarray(i)
-    X = np.hstack((X, np.transpose([i])))
+    return features
 
-print(model.score(X, target))
+
+trained_model = logModel(feature_list(close, 10), get_labels(close)[5:])
+
+
+def test_model(model, test_data):
+
+    features = feature_list(test_data, 10)
+
+    x = np.zeros((len(test_data)-5, 1))
+    for i in features:
+        i = np.asarray(i)
+        x = np.hstack((x, np.transpose([i])))
+
+    print("Model was accurate on " + str(round(model.score(x, get_labels(test_data)[5:]), 4) * 100)
+          + "% of the test samples")
+
+
+test_model(trained_model, test)
+
+# BREAK INTO TEST AND TRAIN. TEST SHOULD BE FINAL X DAYS

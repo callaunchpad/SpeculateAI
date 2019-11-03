212bca6651fbdc5c93577dddb69def4158ce2494
Sophia Song <sophies1234@gmail.com>
2019-11-03 14:19:03 -0800

    vectorize

--- src/Pipeline.py
+++ src/Pipeline.py
@@ -1,4 +1,15 @@
+import pickle
 import gensim
+
+model = gensim.models.KeyedVectors.load_word2vec_format('/Users/sophiasong/Desktop/GoogleNews-vectors-negative300.bin', binary=True)
+word2vec = model.wv #holds the mapping between words and embeddings
+#model.wv.save_word2vec_format('word2vec.bin', binary = True)
+#vocab = model.vocab
+with open('vectors.bin', 'wb') as v:
+	pickle.dump(word2vec, v)
+	v.close()
+
+del model
 
 '''
     Tokens:
@@ -15,8 +26,17 @@ def tokenize(s, arr_len=100):
     tokenizedArr += ["END"]
     return tokenizedArr
 
+
 def vectorize(arr):
-    return arr
+	"""Takes in a tokenized list of words, returns a list of word2vec vectors"""
+    file = 
+    vectors = []
+    for word in arr:
+    	if word != 'pad' :
+    		pickle_in = open('vectors.bin', "rb")
+    		dictionary = pickle.load(pickle_in)
+    		vectors.append(dictionary[word])
+    return vectors
 
 def label(arr):
     return arr

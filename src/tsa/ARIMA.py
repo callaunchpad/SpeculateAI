7480744fee0daf5a10bed289fe1c9877cbc1cc06
Kevin <chevin_ken@berkeley.edu>
2019-12-01 22:50:41 -0800

    Saving and loading model functionality done, regression done

--- src/tsa/ARIMA.py
+++ src/tsa/ARIMA.py
@@ -4,46 +4,66 @@ from matplotlib import pyplot as plt
 from pandas.plotting import autocorrelation_plot
 from sklearn.metrics import mean_squared_error
 from features import *
-import pyramid as pm
+import pickle
 from pyramid.arima import auto_arima
 import glob
 
 def main():
-    # data = pd.read_csv("../../data/DJIA_table.csv", header=0, index_col=0)
-    # data = pd.read_table("../../data/Stocks/fox.us.txt", delimiter=',', header=0, index_col=0)
-    # data = data.reindex(index=data.index[::-1])
-    # series = data["Close"]
-    # first_order = difference(series)
-    # second_order = difference(first_order)
+    data = pd.read_csv("../../data/DJIA_table.csv", header=0, index_col=0)
+    data = data.reindex(index=data.index[::-1])
+    series = data["Close"]
     # model_eval(series)
-    # autocorrelation_plot(series)
-    # plt.show()
-    # plt.plot(series.values)
-    # plt.title("Dow Jones")
-    # plt.show()
-    # plt.plot(first_order.values)
-    # plt.title("First order differencing")
-    # plt.show()
-    # plt.plot(second_order.values)
-    # plt.title("Second order differencing")
-    # plt.show()
-    # autocorrelation_plot(series)
-    # pyplot.show()
-    # model, correct, error = model_eval(series.values)
     stock_dict = read_entire_market()
-    models, percentage, error, percentage_up, error_up, percentage_down, error_down = train_on_market(stock_dict)
-    print("Percentage correctly labeled: ", percentage, "Total error: ", error)
-    print("Percentage when market went up: ", percentage_up, "Total error: ", error_up)
-    print("Percentage when market went down: ", percentage_down, "Total error: ", error_down)
-# def difference(dataset, interval=1):
-# 	diff = list()
-# 	for i in range(interval, len(dataset)):
-# 		value = dataset[i] - dataset[i - interval]
-# 		diff.append(value)
-# 	return pd.Series(diff)
+    save_all_models(stock_dict)
+    # models, percentage, error, percentage_up, error_up, percentage_down, error_down = train_on_market(stock_dict)
+    # print("Percentage correctly labeled: ", percentage, "Total error: ", error)
+    # print("Percentage when market went up: ", percentage_up, "Total error: ", error_up)
+    # print("Percentage when market went down: ", percentage_down, "Total error: ", error_down)
 
+
+
+#Reads the entire stocks dataset into a dictionary with key: stock name, value: values array
+def read_entire_market():
+    stock_path = "../../data/Stocks/"
+    all_stocks = glob.glob(stock_path + "/*.txt")
+    stock_dict = {}
+    count = 0
+    for filename in all_stocks:
+        count += 1
+        try:
+            stock = pd.read_table(filename.replace(".us.txt", ""), delimiter=',', header=0, index_col=0)
+            data = list(reversed(stock["Close"].values))
+            stock_dict[filename] = data
+        except:
+            continue
+    print("count", count)
+    return stock_dict
+#Loop to save all the arima models for each stock in the Stocks dataset
+def save_all_models(stock_dict):
+    for stock in stock_dict:
+        try:
+            series = stock_dict[stock]
+            model = auto_arima(series[:len(series -1)], disp=-1, suppress_warnings=True)
+            save_model(model, stock)
+        except:
+            print("Error in saving model for stock: ", stock)
+            continue
+
+#Loop to load all saved models
+def load_all_models():
+    model_path = "arima_models/"
+    all_models = glob.glob(model_path + "/*.sav")
+    model_dict = {}
+    for filename in all_models:
+        model = pickle.load(open(filename, "rb"))
+        stock_name = filename.replace(".sav", "")
+        model_dict[stock_name] = model
+    return model_dict
+
+#Training loop to determine classification accuracy on entire market
 def train_on_market(stock_dict):
     models = {}
+    iterations = 0
     total_correct = 0
     total_error = 0
     evaluated = 0
@@ -54,6 +74,8 @@ def train_on_market(stock_dict):
     total_error_up = 0
     total_error_down = 0
     for stock in stock_dict:
+        print("Iteration: ", iterations)
+        iterations += 1
         print("Stock: ", stock)
         try:
             model, correct, error, label = model_eval(stock_dict[stock])
@@ -85,28 +107,13 @@ def train_on_market(stock_dict):
     error_down = total_error_down /total_down
     return models, percentage, error, percentage_up, error_up, percentage_down, error_down
 
-def read_entire_market():
-    stock_path = "../../data/Stocks/"
-    all_stocks = glob.glob(stock_path + "/*.txt")
-    stock_dict = {}
-    count = 0
-    for filename in all_stocks:
-        count += 1
-        try:
-            stock = pd.read_table(filename, delimiter=',', header=0, index_col=0)
-            data = list(reversed(stock["Close"].values))
-            stock_dict[filename] = data
-        except:
-            continue
-    print("count", count)
-    return stock_dict
-
+#Evaluates the classification accuracy of a model with one forecast
 def model_eval(values):
     train, test = values[0:len(values)-1], values[len(values)-1]
     predictions = list()
     #labels 0 is go down, 1 is go up
     previous = train[len(train)-1]
-    model = auto_arima(train)
+    model = auto_arima(train, disp=-1, suppress_warnings=True)
     print(model.get_params()["order"])
     prediction = model.predict(1)[0]
     classification = 0
@@ -120,25 +127,52 @@ def model_eval(values):
     if (classification == true):
         correct = 1
         yes = True
-    error = (abs(test - prediction)/prediction) * 100
+    error = (abs(test - prediction)/test) * 100
     print("Predicted: ", prediction, "Actual: ", test, "Previous: ", previous, "Percent error: ", error)
     print("Pred label: ", classification, "True label: ", true, "Correct? ", yes)
     return model, correct, error, true
 
+#Given an already fitted model, make a forecast of the value of the stock at the next day
+def forecast(model):
+    return model.predict(1)[0]
 
-    # model = ARIMA(series, order=(5, 1, 0))
-    # model_fit = model.fit(disp=0)
-    # print(model_fit.summary())
-    # residuals = DataFrame(model_fit.resid)
-    # residuals.plot()
-    # pyplot.show()
-    # residuals.plot(kind='kde')
-    # pyplot.show()
-    # print(residuals.describe())
-    # series.plot()
-    # pyplot.show()
-    # autocorrelation_plot(series)
-    # pyplot.show()
+
+#Test for projecting multiple time steps with arima model
+def projection_test(values):
+    split = int(0.75 * len(values))
+    train, test = values[0:split], values[split:]
+    model = auto_arima(train, disp=-1, suppress_warnings=True)
+    prediction = model.predict(len(test))
+    plt.plot(test)
+    plt.plot(prediction)
+    plt.show()
+
+#Test for saving a single model
+def save_model(model, stock_name):
+    filename = "arima_models/" + stock_name + ".sav"
+    pickle.dump(model, open(filename, 'wb'))
+
+#Test for loading a single model
+def load_model(stock_name):
+    filename = "arima_models/" + stock_name + ".sav"
+    model = pickle.load(open(filename, 'rb'))
+    return model
+
+
+#Computes first order differencing of a series
+def difference(dataset, interval=1):
+	diff = list()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return pd.Series(diff)
+
+#Test for saving and loading a single model
+def test_save_load(series):
+    model = auto_arima(series[:len(series) - 1], disp=-1, suppress_warnings=True)
+    save_model(model, "DJIA")
+    load_model("DJIA")
+    print(model.predict(1)[0])
 
 if __name__== "__main__":
     main()
